ca4349fcdf48bf47fe73b903b016161bcd4f6aa4
Nikola Topalović <topalovicnikola45@gmail.com>
2025-03-25 17:26:00 +0100

    Added CRD in admin.py on USERS and update will be left on user itself to do.

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ from app.database import SessionLocal, engine
 from app.models import Base
 from app.routes.auth import router as auth_router
 from app.routes.tasks import router as tasks_router
+from app.routes.admin import router as admin_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(tasks_router, prefix="/task", tags=["task"])
-
+app.include_router(admin_router, prefix="/admin", tags=['admin'])
 
 Base.metadata.create_all(bind=engine)
 
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,1 +1,62 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from app.models import User, Task
+from app.database import SessionLocal
+from app.routes.auth import get_current_user
+from app.schemas import UserCreate, UserUpdate, TaskCreate, TaskUpdate 
+
+router = APIRouter(
+
+    tags=['admin']
+)
+
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[str, Depends(get_current_user)]
+
+
+
+@router.get('/users')
+async def get_users(db : db_dependency, user : user_dependency):
+
+    if user.get('role') == 'admin':
+        return db.query(User).all()
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not an admin! ")
+
+
+
+@router.get("/user/{user_id}")
+async def get_user(db : db_dependency, user : user_dependency, user_id : int):
+
+    user_to_return = db.query(User).filter(User.id == user_id).first()
+    if not user_to_return:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if user.get('role') == 'admin':
+        return user_to_return
+    else:
+        raise HTTPException(status_code=403, detail="User is not an admin")
+    
+
+
+@router.delete('/user/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(db : db_dependency, user : user_dependency, user_id : int):
+    user_to_delete = db.query(User).filter(User.id == user_id).first()
+
+    if not user_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    if user.get('role') == 'admin':
+        db.delete(user_to_delete)
+        db.commit()
+        return {"message" : "User successfully deleted"}
+    
+    raise HTTPException(status_code=403, detail="User is not an admin!")
