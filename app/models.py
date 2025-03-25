30914dd206e43c47252ad21eff4148c582ed0d55
Nikola Topalović <topalovicnikola45@gmail.com>
2025-03-25 13:35:21 +0100

    Added alembic and also modified columns in models.py

--- app/models.py
+++ app/models.py
@@ -7,23 +7,24 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password=Column(String)
-    role = Column(String,  default="user")
-    is_active = Column(Boolean, default=True)
-    tasks = relationship("Task", back_populates="owner")
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="user", nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    
+    tasks = relationship("Task", back_populates="owner", cascade="all, delete")
 
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String, index=True, nullable=False)
     description = Column(String, index=True)
-    completed = Column(Boolean, default=False)
+    completed = Column(Boolean, default=False, nullable=False)
     deadline = Column(DateTime, default=datetime.utcnow)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    
-    owner = relationship("User", back_populates="tasks")
+    created_at = Column(DateTime, default=datetime.utcnow)  # Dodali smo kreiran datum
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
+    owner = relationship("User", back_populates="tasks")
