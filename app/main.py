68fc14d2d9833969d35cd91d0765a5034821c1ee
Nikola Topalović <topalovicnikola45@gmail.com>
2025-03-26 11:09:27 +0100

    Added delete and update method for user.Tested it out in swagger both routes works.Also included them in main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ from app.models import Base
 from app.routes.auth import router as auth_router
 from app.routes.tasks import router as tasks_router
 from app.routes.admin import router as admin_router
+from app.routes.users import router as users_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(tasks_router, prefix="/task", tags=["task"])
 app.include_router(admin_router, prefix="/admin", tags=['admin'])
+app.include_router(users_router, prefix="/users", tags=['users'])
 
 Base.metadata.create_all(bind=engine)
 
--- app/routes/users.py
+++ app/routes/users.py
@@ -6,8 +6,9 @@ from app.schemas import UserUpdate
 from sqlalchemy.orm import Session
 from app.models import Task,User
 
+
 router = APIRouter(
-    tags=['user']
+    tags=['users']
 )
 
 def get_db():
@@ -18,10 +19,45 @@ def get_db():
         db.close()
 
 
+@router.put('/user/{user_id}')
+async def update_user(user_id: int, user_update: UserUpdate, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
 
-db_dependency = Annotated(Session, Depends(get_db))
-user_dependency = Annotated(str, Depends(get_current_user))
+    print(f"Authenticated user: {user}")  
+    print(f"Trying to update user with ID: {user_id}")
+
+    user_to_update = db.query(User).filter(User.id == user_id).first()
+
+    if not user_to_update:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user.get('role') == 'admin' or user.get('id') == user_id:
+            for key, value in user_update.dict(exclude_unset=True).items():
+                setattr(user_to_update, key, value)
+    else:
+        raise HTTPException(status_code=403, detail="Not authorized to update this user")
+    
+    db.commit()
+    db.refresh(user_to_update)
+
+    return {"message": "User updated successfully"}
 
 
+@router.delete('/user/{user_id}')
+async def delete_user(user_id : int, user : dict = Depends(get_current_user), db : Session = Depends(get_db)):
+
+    user_to_delete = db.query(User).filter(User.id == user_id).first()
+
+    if not user_to_delete:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user.get('role') != 'admin' and user.get('id') != user_id:
+        raise HTTPException(status_code=403, detail="Not authorized to delete this user")
+    
+    db.delete(user_to_delete)
+    db.commit()
+
+    return {"message": "User deleted successfully"}
+
+    
 
 
