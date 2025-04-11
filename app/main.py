318048d0fb66ba7c75b5e158f0e97082f5cbc8d8
Nikola Topalović <topalovicnikola45@gmail.com>
2025-04-11 17:21:50 +0200

    Imported middleware in main.py for REACT frontend and defined a test get route to test out my frontend

--- app/main.py
+++ app/main.py
@@ -5,14 +5,26 @@ from app.routes.auth import router as auth_router
 from app.routes.tasks import router as tasks_router
 from app.routes.admin import router as admin_router
 from app.routes.users import router as users_router
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI() 
 
-app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
+@app.get("/")
+async def test():
+    return {"Hello!"}
+
+app.include_router(auth_router,  prefix="/auth", tags=["auth"])
 app.include_router(tasks_router, prefix="/task", tags=["task"])
 app.include_router(admin_router, prefix="/admin", tags=['admin'])
-app.include_router(users_router, prefix="/users", tags=['users'])
+app.include_router(users_router, prefix="/users",tags=['users'])
 
 Base.metadata.create_all(bind=engine)
 
